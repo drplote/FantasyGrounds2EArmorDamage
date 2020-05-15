250ab3283b627548ee1bbe69b7540bce7b265503
drplote <drplote@gmail.com>
2020-05-14 23:00:53 -0400

    committing before I delete some stuff I think is redundant

--- campaign/scripts/char_main_armor_damage.lua
+++ campaign/scripts/char_main_armor_damage.lua
@@ -3,18 +3,21 @@ function onInit()
 	super.onInit();
 	
 	local node = getDatabaseNode();
-	DB.addHandler(DB.getPath(nodeChar, "inventorylist.*.carried"), "onUpdate", updateArmor);
-	DB.addHandler(DB.getPath(nodeChar, "inventorylist.*.hplost"), "onUpdate", updateArmor);
+	DB.addHandler(DB.getPath(node, "inventorylist.*.carried"), "onUpdate", updateArmor);
+	DB.addHandler(DB.getPath(node, "inventorylist.*.hplost"), "onUpdate", updateArmor);
+	DB.addHandler(DB.getPath(node, "inventorylist.*.maxhp"), "onUpdate", updateArmor);
 	
 end
 
 function update()
 	super.update();
 	local nodeRecord = getDatabaseNode();
-	DB.removeHandler(DB.getPath(nodeChar, "inventorylist.*.carried"), "onUpdate", updateArmor);
-	DB.removeHandler(DB.getPath(nodeChar, "inventorylist.*.hplost"), "onUpdate", updateArmor);
+	DB.removeHandler(DB.getPath(nodeRecord, "inventorylist.*.carried"), "onUpdate", updateArmor);
+	DB.removeHandler(DB.getPath(nodeRecord, "inventorylist.*.hplost"), "onUpdate", updateArmor);
+	DB.removeHandler(DB.getPath(nodeRecord, "inventorylist.*.maxhp"), "onUpdate", updateArmor);
 end
 
 function updateArmor()
+	Debug.console("char_main_armor_damage.lua", "updateArmor");
 	CharManager.calcItemArmorClass(getDatabaseNode());
 end
